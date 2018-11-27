486be05fbbf1d8cd5d548a77af6877b8edc738ae
Sam Lai <sam.lai@udnshopping.com>
2018-11-27 15:04:22 +0800

    Module: UDNSAuthorizer User: sam.lai@udnshopping.com Issue: created source control. Note: none.

---
+++ app/src/main/java/com/udnshopping/udnsauthorizer/MainActivity.kt
@@ -0,0 +1,72 @@
+package com.udnshopping.udnsauthorizer
+
+import android.content.Intent
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import com.marcelkliemannel.kotlinonetimepassword.HmacAlgorithm
+import com.marcelkliemannel.kotlinonetimepassword.TimeBasedOneTimePasswordConfig
+import com.marcelkliemannel.kotlinonetimepassword.TimeBasedOneTimePasswordGenerator
+import org.apache.commons.codec.binary.Base32
+import java.util.*
+import android.net.Uri
+import android.support.v7.widget.LinearLayoutManager
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    private var textResults: TextView? = null
+
+    // Initializing an empty ArrayList to be filled with animals
+    val secrets: ArrayList<String> = ArrayList()
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        textResults = findViewById<TextView>(R.id.textView2) as TextView
+
+        // Creates a vertical Layout Manager
+        my_recycler_view.layoutManager = LinearLayoutManager(this)
+
+        // You can use GridLayoutManager if you want multiple columns. Enter the number of columns as a parameter.
+//        rv_animal_list.layoutManager = GridLayoutManager(this, 2)
+
+        // Access the RecyclerView Adapter and load the data into it
+        my_recycler_view.adapter = SecretAdapter(secrets, this)
+    }
+    fun scan(view: View) {
+        val intent = Intent(this, ScanActivity::class.java).apply {
+
+        }
+        startActivityForResult(intent, 1)
+    }
+
+    fun addSecret(secret: String) {
+        secrets.add(secret)
+    }
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        val auth = data?.extras?.getString("auth")
+
+        val uri = Uri.parse(auth)
+
+
+        val secret = uri.getQueryParameter("secret")
+        val config = TimeBasedOneTimePasswordConfig(codeDigits = 6, hmacAlgorithm = HmacAlgorithm.SHA1,
+            timeStep = 30, timeStepUnit = java.util.concurrent.TimeUnit.SECONDS)
+        val timeBasedOneTimePasswordGenerator = TimeBasedOneTimePasswordGenerator(Base32().decode(secret), config)
+        textResults?.setText(timeBasedOneTimePasswordGenerator.generate())
+        addSecret(timeBasedOneTimePasswordGenerator.generate())
+
+        // Creates a vertical Layout Manager
+        my_recycler_view.layoutManager = LinearLayoutManager(this)
+
+        // You can use GridLayoutManager if you want multiple columns. Enter the number of columns as a parameter.
+//        rv_animal_list.layoutManager = GridLayoutManager(this, 2)
+
+        // Access the RecyclerView Adapter and load the data into it
+        my_recycler_view.adapter = SecretAdapter(secrets, this)
+    }
+}
---
+++ app/src/main/java/com/udnshopping/udnsauthorizer/ScanActivity.kt
@@ -0,0 +1,103 @@
+package com.udnshopping.udnsauthorizer
+
+import android.annotation.SuppressLint
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Bundle
+import android.support.v4.app.ActivityCompat
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity;
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.widget.TextView
+import com.google.android.gms.vision.CameraSource
+import com.google.android.gms.vision.Detector
+import com.google.android.gms.vision.barcode.Barcode
+import com.google.android.gms.vision.barcode.BarcodeDetector
+import java.io.IOException
+
+class ScanActivity : AppCompatActivity() {
+    private var cameraView: SurfaceView? = null
+    private var textResults: TextView? = null
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        if (ContextCompat.checkSelfPermission(applicationContext,
+                android.Manifest.permission.CAMERA)
+            != PackageManager.PERMISSION_GRANTED) {
+            // Permission is not granted
+            // Should we show an explanation?
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this@ScanActivity,
+                    android.Manifest.permission.CAMERA)) {
+                // Show an explanation to the user *asynchronously* -- don't block
+                // this thread waiting for the user's response! After the user
+                // sees the explanation, try again to request the permission.
+            } else {
+                // No explanation needed, we can request the permission.
+                ActivityCompat.requestPermissions(this@ScanActivity,
+                    arrayOf(android.Manifest.permission.CAMERA),
+                    0)
+
+                // MY_PERMISSIONS_REQUEST_READ_CONTACTS is an
+                // app-defined int constant. The callback method gets the
+                // result of the request.
+            }
+        } else {
+            // Permission has already been granted
+            startCamera()
+        }
+    }
+    fun startCamera() {
+        setContentView(R.layout.activity_scan)
+        cameraView = findViewById<SurfaceView>(R.id.surfaceView) as SurfaceView
+        textResults = findViewById<TextView>(R.id.textView) as TextView
+        val detector = BarcodeDetector.Builder(applicationContext)
+            .setBarcodeFormats(Barcode.ALL_FORMATS)
+            .build()
+        val cameraSource = CameraSource.Builder(this, detector)
+            .setRequestedPreviewSize(1600, 1024)
+            .setAutoFocusEnabled(true) //you should add this feature
+            .build()
+        println(cameraView?.holder)
+        cameraView?.holder?.addCallback(object : SurfaceHolder.Callback {
+            @SuppressLint("MissingPermission")
+            override fun surfaceCreated(holder: SurfaceHolder) {
+                try {
+                    cameraSource.start(cameraView?.holder)
+                } catch (ex: IOException) {
+                    ex.printStackTrace()
+                }
+
+            }
+            override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+
+            }
+            override fun surfaceDestroyed(holder: SurfaceHolder) {
+                cameraSource.stop()
+            }
+        })
+        detector.setProcessor(object : Detector.Processor<Barcode> {
+            override
+            fun release() {
+            }
+            override
+            fun receiveDetections(detections: Detector.Detections<Barcode>) {
+                val barcodes = detections.getDetectedItems();
+                if (barcodes.size() != 0) {
+                    val intent = Intent(this@ScanActivity,MainActivity::class.java)
+                    intent.putExtra("auth",barcodes.valueAt(0).displayValue)
+                    setResult(1, intent)
+                    this@ScanActivity.finish()
+                }
+            }
+        });
+
+        if (!detector!!.isOperational) {
+            return
+        }
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        startCamera()
+    }
+
+}
