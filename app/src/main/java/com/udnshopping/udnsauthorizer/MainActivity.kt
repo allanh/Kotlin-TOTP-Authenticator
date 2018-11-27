a2469ce0eb0bb67fcd2cd3db45c1e0b4e8938af0
Sam Lai <sam.lai@udnshopping.com>
2018-11-27 16:46:40 +0800

    Module: MainActivity User: sam.lai@udnshopping.com Issue: let list view is full screen. Note: none.

--- app/src/main/java/com/udnshopping/udnsauthorizer/MainActivity.kt
+++ app/src/main/java/com/udnshopping/udnsauthorizer/MainActivity.kt
@@ -3,7 +3,6 @@ package com.udnshopping.udnsauthorizer
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.view.View
 import android.widget.TextView
 import com.marcelkliemannel.kotlinonetimepassword.HmacAlgorithm
 import com.marcelkliemannel.kotlinonetimepassword.TimeBasedOneTimePasswordConfig
@@ -17,8 +16,6 @@ import android.view.Menu
 import android.view.MenuItem
 
 class MainActivity : AppCompatActivity() {
-    private var textResults: TextView? = null
-
     // Initializing an empty ArrayList to be filled with animals
     val secrets: ArrayList<String> = ArrayList()
 
@@ -26,14 +23,8 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        textResults = findViewById<TextView>(R.id.textView2) as TextView
-
         // Creates a vertical Layout Manager
         my_recycler_view.layoutManager = LinearLayoutManager(this)
-
-        // You can use GridLayoutManager if you want multiple columns. Enter the number of columns as a parameter.
-//        rv_animal_list.layoutManager = GridLayoutManager(this, 2)
-
         // Access the RecyclerView Adapter and load the data into it
         my_recycler_view.adapter = SecretAdapter(secrets, this)
     }
@@ -41,23 +32,15 @@ class MainActivity : AppCompatActivity() {
         super.onActivityResult(requestCode, resultCode, data)
 
         val auth = data?.extras?.getString("auth")
-
         val uri = Uri.parse(auth)
-
-
         val secret = uri.getQueryParameter("secret")
         val config = TimeBasedOneTimePasswordConfig(codeDigits = 6, hmacAlgorithm = HmacAlgorithm.SHA1,
             timeStep = 30, timeStepUnit = java.util.concurrent.TimeUnit.SECONDS)
         val timeBasedOneTimePasswordGenerator = TimeBasedOneTimePasswordGenerator(Base32().decode(secret), config)
-        textResults?.setText(timeBasedOneTimePasswordGenerator.generate())
         addSecret(timeBasedOneTimePasswordGenerator.generate())
 
         // Creates a vertical Layout Manager
         my_recycler_view.layoutManager = LinearLayoutManager(this)
-
-        // You can use GridLayoutManager if you want multiple columns. Enter the number of columns as a parameter.
-//        rv_animal_list.layoutManager = GridLayoutManager(this, 2)
-
         // Access the RecyclerView Adapter and load the data into it
         my_recycler_view.adapter = SecretAdapter(secrets, this)
     }
