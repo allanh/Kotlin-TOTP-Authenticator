aded459dc0a81f6bdc58a6e90d28b0064b550684
Sam Lai <sam.lai@udnshopping.com>
2018-11-28 16:25:02 +0800

    Module: UDNSAuthorizer User: sam.lai@udnshopping.com Issue: save PIN into local database. Note: none.

--- app/src/main/java/com/udnshopping/udnsauthorizer/MainActivity.kt
+++ app/src/main/java/com/udnshopping/udnsauthorizer/MainActivity.kt
@@ -3,31 +3,33 @@ package com.udnshopping.udnsauthorizer
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.TextView
 import com.marcelkliemannel.kotlinonetimepassword.HmacAlgorithm
 import com.marcelkliemannel.kotlinonetimepassword.TimeBasedOneTimePasswordConfig
 import com.marcelkliemannel.kotlinonetimepassword.TimeBasedOneTimePasswordGenerator
 import org.apache.commons.codec.binary.Base32
-import java.util.*
 import android.net.Uri
 import android.support.v7.widget.LinearLayoutManager
 import kotlinx.android.synthetic.main.activity_main.*
 import android.view.Menu
 import android.view.MenuItem
+import android.content.Context
+import com.google.gson.Gson
+import com.google.gson.annotations.SerializedName
+import com.google.gson.reflect.TypeToken
 
 class MainActivity : AppCompatActivity() {
     // Initializing an empty ArrayList to be filled with animals
-    val secrets: ArrayList<Pair<String, String>> = ArrayList()
+    private val secrets: MutableList<PIN> = mutableListOf()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        // Creates a vertical Layout Manager
-        my_recycler_view.layoutManager = LinearLayoutManager(this)
-        // Access the RecyclerView Adapter and load the data into it
-        my_recycler_view.adapter = SecretAdapter(secrets, this)
+        val pins = getPinList()
+        secrets += pins
+        adapteSecret()
     }
+
     override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
         super.onActivityResult(requestCode, resultCode, data)
 
@@ -35,23 +37,31 @@ class MainActivity : AppCompatActivity() {
         val uri = Uri.parse(auth)
         val secret = uri.getQueryParameter("secret")
         val user = uri.path
-        val config = TimeBasedOneTimePasswordConfig(codeDigits = 6, hmacAlgorithm = HmacAlgorithm.SHA1,
-            timeStep = 30, timeStepUnit = java.util.concurrent.TimeUnit.SECONDS)
+        val config = TimeBasedOneTimePasswordConfig(
+            codeDigits = 6, hmacAlgorithm = HmacAlgorithm.SHA1,
+            timeStep = 30, timeStepUnit = java.util.concurrent.TimeUnit.SECONDS
+        )
         val timeBasedOneTimePasswordGenerator = TimeBasedOneTimePasswordGenerator(Base32().decode(secret), config)
-        val pin = timeBasedOneTimePasswordGenerator.generate()
-        addSecret(Pair<String, String>(pin, user))
+        val pin: PIN = PIN(timeBasedOneTimePasswordGenerator.generate(), user)
+        addSecret(pin)
+        //--SAVE Data
+        val preferences = getPreferences(Context.MODE_PRIVATE)
+        val editor = preferences.edit()
+        val type = object : TypeToken<List<PIN>>() {}.type
+        val json = Gson().toJson(secrets, type)
+        editor.putString("PINs", json).apply()
+        editor.commit()
 
-        // Creates a vertical Layout Manager
-        my_recycler_view.layoutManager = LinearLayoutManager(this)
-        // Access the RecyclerView Adapter and load the data into it
-        my_recycler_view.adapter = SecretAdapter(secrets, this)
+        adapteSecret()
     }
+
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu to use in the action bar
         val inflater = menuInflater
         inflater.inflate(R.menu.toolbar_menu, menu)
         return super.onCreateOptionsMenu(menu)
     }
+
     override fun onOptionsItemSelected(item: MenuItem) = when (item.itemId) {
         R.id.action_camera -> {
             // User chose the "Settings" item, show the app settings UI...
@@ -64,13 +74,40 @@ class MainActivity : AppCompatActivity() {
             super.onOptionsItemSelected(item)
         }
     }
+
+    data class PIN(
+        @SerializedName("secret") private val _key: String?,
+        @SerializedName("user") private val _value: String?
+    ) {
+        val key: String
+            get() = _key ?: ""
+
+        val value: String
+            get() = _value ?: ""
+    }
+
+    fun getPinList(): List<PIN> {
+        val type = object : TypeToken<List<PIN>>() {}.type
+        val preferences = getPreferences(Context.MODE_PRIVATE)
+        val json = preferences.getString("PINs", "")
+        return if (json.isNotBlank()) Gson().fromJson(json, type) else listOf()
+    }
+
+    fun adapteSecret() {
+        // Creates a vertical Layout Manager
+        my_recycler_view.layoutManager = LinearLayoutManager(this)
+        // Access the RecyclerView Adapter and load the data into it
+        my_recycler_view.adapter = SecretAdapter(secrets, this)
+    }
+
     fun scan() {
         val intent = Intent(this, ScanActivity::class.java).apply {
 
         }
         startActivityForResult(intent, 1)
     }
-    fun addSecret(secret: Pair<String, String>) {
-        secrets.add(secret)
+
+    fun addSecret(pin: PIN) {
+        secrets += pin
     }
 }
--- app/src/main/java/com/udnshopping/udnsauthorizer/SecretAdapter.kt
+++ app/src/main/java/com/udnshopping/udnsauthorizer/SecretAdapter.kt
@@ -6,13 +6,16 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import kotlinx.android.synthetic.main.secret_list_item.view.*
-class SecretAdapter(val items : ArrayList<Pair<String, String>>, val context: Context) : RecyclerView.Adapter<SecretAdapter.ViewHolder>() {
+class SecretAdapter(val items : List<MainActivity.PIN>?, val context: Context) : RecyclerView.Adapter<SecretAdapter.ViewHolder>() {
     class ViewHolder(v: View) : RecyclerView.ViewHolder(v) {
         val tv_secret_type: TextView = v.findViewById(R.id.tv_secret_type)
         val tv_user_type: TextView = v.findViewById(R.id.tv_user_type)
     }
     override fun getItemCount(): Int {
-        return items.size
+        if (items != null) {
+            return items.size
+        }
+        return 0
     }
     // Inflates the item views
     override fun onCreateViewHolder(p0: ViewGroup, p1: Int): ViewHolder {
@@ -20,7 +23,8 @@ class SecretAdapter(val items : ArrayList<Pair<String, String>>, val context: Co
     }
     // Binds each animal in the ArrayList to a view
     override fun onBindViewHolder(p0: ViewHolder, p1: Int) {
-        p0?.tv_secret_type?.text = items.get(p1).first
-        p0?.tv_user_type?.text = items.get(p1).second
+        val pin = items?.get(p1)
+        p0?.tv_secret_type?.text = pin?.key
+        p0?.tv_user_type?.text = pin?.value
     }
 }
