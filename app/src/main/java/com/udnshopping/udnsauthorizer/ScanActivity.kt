cd7277aee49645c52582386053908e91ff6f8cc1
Sam Lai <sam.lai@udnshopping.com>
2018-11-30 15:11:51 +0800

    Module: ScanActivity User: sam.lai@udnshopping.com Issue: Hidden status bar when scan status. Note: none.

--- app/src/main/java/com/udnshopping/udnsauthorizer/ScanActivity.kt
+++ app/src/main/java/com/udnshopping/udnsauthorizer/ScanActivity.kt
@@ -24,6 +24,13 @@ class ScanActivity : AppCompatActivity() {
     private var cameraView: SurfaceView? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        // Hide the status bar.
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+        // Remember that you should never show the action bar if the
+        // status bar is hidden, so hide that too if necessary.
+        actionBar?.hide()
+
         if (ContextCompat.checkSelfPermission(
                 applicationContext,
                 android.Manifest.permission.CAMERA
