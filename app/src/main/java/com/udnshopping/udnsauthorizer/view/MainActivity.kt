edefd001f415db1c5b3ed0c7d19d6902ed32585a
Allan Shih <allan.shih@udnshopping.com>
2019-04-03 16:14:02 +0800

    [bugfix] Fix the index out of bound issue.

--- app/src/main/java/com/udnshopping/udnsauthorizer/view/MainActivity.kt
+++ app/src/main/java/com/udnshopping/udnsauthorizer/view/MainActivity.kt
@@ -147,7 +147,8 @@ class MainActivity : DaggerAppCompatActivity() {
         super.onRequestPermissionsResult(requestCode, permissions, grantResults)
         if (requestCode == CAMERA_REQUEST_CODE) {
 
-            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+            if (grantResults.isNotEmpty() && grantResults.isNotEmpty() &&
+                grantResults[0] == PackageManager.PERMISSION_GRANTED) {
                 scan()
             } else {
                 if (!ActivityCompat.shouldShowRequestPermissionRationale(this,
